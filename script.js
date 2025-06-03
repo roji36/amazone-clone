502071c3689fad8f098f07c9bb5bab6a9ccdb70a
roji36 <rojikc353@gmail.com>
2025-06-03 10:26:35 +0545

    final project update

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const products = [
     { id: 1, name: "Wireless Headphone", price: 2000, image: "images/headphone.jpg" },
     { id: 2, name: "iPhone 17 Pro Max", price: 250000, image: "images/phone.jpg" },
-    { id: 2, name: "iWatch", price: 89000, image: "images/iWatch.jpg" },
-    { id: 2, name: "Speaker", price: 1000, image: "images/speaker.jpg" },
-    { id: 2, name: "Macbook Pro M4", price:349000, image: "images/laptop.jpg" }
+    { id: 3, name: "iWatch", price: 89000, image: "images/iwatch.jpg" },
+    { id: 4, name: "Speaker", price: 1000, image: "images/speaker.jpg" },
+    { id: 5, name: "Macbook Pro M4", price:349000, image: "images/laptop.jpg" },
 ];
 
 let cart = JSON.parse(localStorage.getItem("cart")) || [];
